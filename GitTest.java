d36fb356c5f66e3f515b6c3f4b1e293b13cfc987
Nikolas Beltran <nbeltran14@mail.wou.edu>
2016-09-19 11:30:42 -0700

    This is a test commit.

---
+++ GitTest.java
@@ -0,0 +1,17 @@
+public class GitTest
+{
+	public GitTest()
+	{
+		printMessage();
+	}
+	
+	public void printMessage()
+	{
+		System.out.println("This is a test message.");
+	}
+	
+	public static void main(String args[])
+	{
+		GitTest test = new GitTest();
+	}
+}
